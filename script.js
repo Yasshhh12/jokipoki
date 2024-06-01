307b1fd99703cab62a489241d3fe510a649400e4
Yasshhh12 <prasyast152@gmail.com>
2024-06-02 03:33:44 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+// async function getAllUsers(){
+//     try {
+
+//         const response = await fetch(" https://jsonplaceholder.typicode.com/users");
+
+//         const data = await response.json();
+//         console.log(data);
+
+
+//     } catch (error) {
+//         console.log("Error detected");
+//     }
+// }
+
+// getAllUsers();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch(()=>{
+    console.log("Error");
+})
